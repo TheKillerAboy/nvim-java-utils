789fa8bec1c30be1c413966b50db312f93069afe
Annekin Meyburgh <aboy001@outlook.com>
2023-05-19 12:53:19 +0200

    Working lua native bootstrap and get java source

---
+++ lua/nvim-java-utils/bootstrap.lua
@@ -0,0 +1,119 @@
+local M = {}
+
+local execute = function(opts, cmd)
+  os.execute('cd ' .. opts.plugin_root_dir .. ';' .. cmd)
+end
+
+local luarocks_install = function(opts)
+  local rockspec = opts.rockspec
+
+  execute(opts, 'luarocks --tree lua_modules install --only-deps ' .. rockspec)
+end
+
+local get_luarocks_deps_root = function(opts)
+  opts.luarocks_deps_root = opts.plugin_root_dir .. '/lua_modules/share/lua'
+  for name, _ in vim.fs.dir(opts.luarocks_deps_root) do
+    opts.luarocks_deps_root = opts.luarocks_deps_root .. '/' .. name
+    goto continue
+  end
+
+  ::continue::
+
+  return opts
+end
+
+local get_deps_files = function(opts)
+  local deps = {}
+
+  for name, type in vim.fs.dir(opts.luarocks_deps_root, {depth=math.huge}) do
+    table.insert(deps, {name = name, type = type})
+  end
+
+  return deps
+end
+
+local get_plugin_build_lua_path = function()
+  return debug.getinfo(3,"S").source:sub(2);
+end
+
+local get_plugin_dir = function(opts)
+  opts.plugin_lua_dir = vim.fs.find('lua', {
+      path = opts.plugin_build_lua_path,
+      upward = true,
+      type = 'directory'
+    }
+  )[1]
+
+  opts.plugin_root_dir = vim.fs.dirname(opts.plugin_lua_dir)
+
+  return opts
+end
+
+local has_lockfile = function(opts)
+  local lock_files = vim.fs.find('.luarocks_bootstrap.lock',
+    {
+      path = opts.plugin_lua_dir,
+      type = 'file'
+    }
+  )
+
+  return #lock_files == 1
+end
+
+local luarocks_cp = function(opts, deps)
+  for _, dep in ipairs(deps) do
+    if dep.type == 'file' then
+      execute(opts, 'cp ' .. opts.luarocks_deps_root .. '/' .. dep.name .. ' ' .. opts.plugin_lua_dir .. '/' .. dep.name)
+    else
+      execute(opts, 'mkdir ' .. opts.plugin_lua_dir .. '/' .. dep.name)
+    end
+  end
+end
+
+local create_lockfile_cnt = function(opts, deps)
+  local lock_cnt = {}
+
+  for _, dep in ipairs(deps) do
+    table.insert(lock_cnt, opts.plugin_lua_dir .. '/' .. dep.name)
+  end
+
+  table.insert(lock_cnt, opts.plugin_lua_dir .. '/.luarocks_bootstrap.lock')
+
+  return table.concat(lock_cnt, '\n')
+end
+
+
+local write_lockfile = function(opts, deps)
+  local cnt = create_lockfile_cnt(opts, deps)
+
+  local handle = io.open(opts.plugin_lua_dir .. '/.luarocks_bootstrap.lock', 'w')
+  io.output(handle)
+  io.write(cnt)
+  io.close(handle)
+end
+
+function M.bootstrap(opts)
+  opts.plugin_build_lua_path = get_plugin_build_lua_path()
+  opts = get_plugin_dir(opts)
+
+  if has_lockfile(opts) then
+    return  
+  end
+
+  luarocks_install(opts)
+  opts = get_luarocks_deps_root(opts)
+  deps = get_deps_files(opts)
+  luarocks_cp(opts, deps)
+  write_lockfile(opts, deps)
+end
+
+function M.uninstall(opts)
+  opts.plugin_build_lua_path = get_plugin_build_lua_path()
+  opts = get_plugin_dir(opts)
+
+  if has_lockfile(opts) then
+    execute(opts, 'cat ' .. opts.plugin_lua_dir .. '/.luarocks_bootstrap.lock' .. ' | xargs rm -rf')
+  end
+end
+
+return M
--- lua/nvim-java-utils/init.lua
+++ lua/nvim-java-utils/init.lua
@@ -1,14 +1,39 @@
 local M = {}
 
-local src = require("nvim-java-utils.src")
+function M.build()
+  local opts = {rockspec = 'nvim-java-utils-dev-1.rockspec'}
+  require('nvim-java-utils.bootstrap').bootstrap(opts)
+end
+
+
+function M.remove()
+  local opts = {rockspec = 'nvim-java-utils-dev-1.rockspec'}
+  require('nvim-java-utils.bootstrap').uninstall(opts)
+end
+
+local function create_user_commd(logger, name, fnc, opts)
+  logger:info(string.format("Adding User Commmand(:%s)", name))
+  vim.api.nvim_create_user_command(
+    name,
+    function(_)
+      logger:info(string.format("Calling Command(:%s)", name))
+      fnc()
+    end,
+    opts
+  )
+end
 
 function M.setup()
-  vim.notify("SETUP", vim.log.levels.INFO, nil)
-  vim.api.nvim_create_user_command(
-    'JavaUtilsGetSrc',
-    function(_)
-      src.get_src_dir()
-    end,
+  local logger = require("nvim-java-utils.log").getLogger()
+  local src = require("nvim-java-utils.src")
+
+  logger:logLine(logger.INFO)
+  logger:info("Setup nvim-java-utils")
+
+  create_user_commd(
+    logger,
+    'JavaUtils',
+    function()end,
     {nargs = 0}
   )
 end
